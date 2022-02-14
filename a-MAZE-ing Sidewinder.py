6ed73ec5aaaabadbc05fc5fca948c63f65d65528
Marcel Noller <96265697+MaNoller@users.noreply.github.com>
2022-02-14 20:18:16 +0100

    Update a-MAZE-ing Sidewinder.py
    Bugfix

--- a-MAZE-ing Sidewinder.py
+++ a-MAZE-ing Sidewinder.py
@@ -6,34 +6,56 @@ import time
 width = 10
 height = 10
 margin = 3
-dims = [10,10]
+dims = [4,4]
 # print('dims1:' ,dims[1])
 size = (dims[1] * (width + margin) + margin, dims[0] * (height + margin) + margin)
 
 
+WallGrid=np.full([dims[0], dims[1]],'', dtype=object)
 
-def debuild_walls(x,y,nx,ny):
+
+def draw_dirs(x,y,nx,ny):
+
+    if x!=nx:
+        if x>nx:
+            WallGrid[x][y]+=('U')
+            WallGrid[nx][y]+=('D')
+        else:
+            WallGrid[x][y]+=('D')
+            WallGrid[nx][y]+=('U')
+
+    else:
+        if y<ny:
+            WallGrid[x][y]+=('R')
+            WallGrid[x][ny]+=('L')
+        else:
+            WallGrid[x][y]+=('L')
+            WallGrid[x][ny]+=('R')
+    print(WallGrid)
+
+
+
+def debuild_walls(x,y,nx,ny): #nx=row, ny=column
     #print(x,y,nx,ny)
     if x != nx:
         if x > nx:
             pygame.draw.rect(screen, GREEN,
-                             (margin + nx * (width + margin), margin + y * (height + margin), 2 * width + margin,
-                              height))
+                             ( margin + y * (width + margin), margin + nx * (height + margin),width,
+                              2*height+margin))
         else:
-            #print('aaa', x,width,margin)
             pygame.draw.rect(screen, GREEN,
-                             (margin + x * (width + margin), margin + y * (height + margin), 2 * width + margin, height))
+                             (margin + y * (width + margin),margin + x * (height + margin),   width,
+                              2*height+margin))
 
     else:
-        print(x,y,nx,ny)
         if y > ny:
-            print('joo')
             pygame.draw.rect(screen, GREEN,
-                             (margin + x * (width + margin), margin + ny * (height + margin), width,
-                              2 * height + margin))
+                             ( margin + ny * (width + margin),margin + x * (height + margin), 2*width+margin,
+                              height))
         else:
             pygame.draw.rect(screen, GREEN,
-                             (margin + x * (width + margin), margin + y * (height + margin), width, 2 * height + margin))
+                             (margin + y * (width + margin),margin + x * (height + margin),  2*width+margin,
+                              height))
 
 
 
@@ -52,19 +74,17 @@ pygame.display.set_caption("My Game")
 # grid=[[1 for x in range(10)]for y in range(10)]
 
 grid = np.zeros((dims[0], dims[1]))
-start_x = np.random.randint(grid.shape[0])
-start_y = np.random.randint(grid.shape[1])
-grid[start_x][start_y] = 1
+
 
 recolor = WHITE
 
 for row in range(dims[0]):
     for column in range(dims[1]):
         recolor = WHITE
-        if grid[column][row] != 0:
-            if grid[column][row] == 1:
+        if grid[row][column] != 0:
+            if grid[row][column] == 1:
                 recolor = GREEN
-            elif grid[column][row] == 2:
+            elif grid[row][column] == 2:
                 recolor = RED
             else:
                 recolor = BLUE
@@ -80,6 +100,7 @@ clock = pygame.time.Clock()
 dir=[0,1]#0 is west, 1 is north
 for el in range(dims[1]-1):
     debuild_walls(el+1,0,el,0)
+    draw_dirs(el+1,0,el,0)
     pygame.display.flip()
 for row in range(1,dims[0]):
 
@@ -97,10 +118,12 @@ for row in range(1,dims[0]):
             cell=random.choice(run)#
             run=list()
             debuild_walls(cell[0],cell[1],cell[0],cell[1]-1)
+            draw_dirs(cell[0],cell[1],cell[0],cell[1]-1)
             pygame.display.flip()
             pass
         elif column<dims[1]:
             debuild_walls(column+1,row,column,row)
+            draw_dirs(column+1,row,column,row)
             pygame.display.flip()
 
 
